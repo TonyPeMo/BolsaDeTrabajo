8e58a040aa1e0b6d666f7476896b56155478b989
Soufian123 <proni10102002@gmail.com>
2023-11-03 15:04:22 +0100

    fet

--- BolsaDeTrabajo/src/main/java/com/example/demo/bean/EmpresaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.bean;
-
-public class EmpresaNotFoundException extends RuntimeException {
-
-	public EmpresaNotFoundException(Long id) {
-		super("Could not find employee " + id);
-	}
-}
--- BolsaDeTrabajo/src/main/java/com/example/demo/bean/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.bean;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-	
-}
-
